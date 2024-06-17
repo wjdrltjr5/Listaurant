acaa0dddf2efbb47abee4186094dfb81a093efef
Jeong gi seok <wjdrltjr5@naver.com>
2024-06-17 11:15:43 +0900

    feat : 프로젝트 시작

---
+++ src/main/java/com/example/listaurant/ListaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.listaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/listaurant/ListaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
