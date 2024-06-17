964533075392e40156cbf64a97036dd75e6ffb5a
Jeong gi seok <wjdrltjr5@naver.com>
2024-06-17 12:41:54 +0900

    feat : jsp dependency add

---
+++ src/main/java/com/example/listaurant/common/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.listaurant.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request ->
+            request.anyRequest().permitAll()
+        );
+
+
+        return http.build();
+    }
+}
