ea86ae16ba5cb503ec39e22920be3cea42e00c51
Jeong gi seok <wjdrltjr5@naver.com>
2024-06-20 15:06:16 +0900

    docs : table_sql update

--- src/main/resources/table_sql.sql
+++ src/main/resources/table_sql.sql
@@ -8,13 +8,22 @@ CREATE TABLE member (
 );
 
 CREATE TABLE txt (
-  txt_id int PRIMARY KEY,
+  txt_id int PRIMARY KEY AUTO_INCREMENT,
   place_name varchar(255),
   written_date DATETIME,
   recommend INT,
   scope INT,
+  lat double,
+  lng double,
   member_id INT,
   text varchar(8000),
   nickname VARCHAR(255),
   FOREIGN KEY (member_id) REFERENCES member(member_id)
+);
+
+create table recommendCheck)
+    txt_id Int,
+    member_id INT,
+    FOREIGN KEY (member_id) REFERENCES member(member_id),
+    FOREIGN KEY (txt_id) REFERENCES txt(txt_id)
 );
